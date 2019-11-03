a8cf0f2dcb4e74af68c137dfbbadc8077701532c
Dmitry <dmitry.tarasenko@computools.com>
2019-11-03 22:45:18 +0200

    added query to User model

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,12 +1,26 @@
 const { Router } = require('express');
 const router = Router();
+const models = require('../models/index');
+// const User = models.Sequelize.User;
 
-router.get('/', (req, res) => {
-  res.json({a: 1});
+router.get('/', async (req, res) => {
+  // try {
+  //   const user = await models.User.create({
+  //     username: 'Vasya2'
+  //   });
+  //   res.status(201).json({
+  //     user
+  //   })
+  // } catch (e) {
+  //   console.log(e);
+  //   res.status(500).json({
+  //     message: 'Server error'
+  //   })
+  // }
 });
 
 router.post('/', (req, res) => {
-
+  
 });
 
 router.put('/:id', (req, res) => {
